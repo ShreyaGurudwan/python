190aa9f89e1dcee79b1bfdfa758b77c9bb6b9720
shreya <shreyagurudwan65@gmail.com>
2025-06-27 09:20:26 +0530

    static file configured in settings .py and static file collected

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,6 +116,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Collect static files here
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Optional: Your app-level static folders
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'yourappname', 'static'),
+]
+
+# Media files (user-uploaded)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
